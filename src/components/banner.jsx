4aec5e0ebd5c3a23d1d92a82a26d9c6966263747
ShrinathMateti <shrinathrmateti@gmail.com>
2025-06-18 11:55:18 +0530

    banners updated;

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,15 +1,12 @@
-import banner from '../assets/pythondeskbanner.jpg';
+import banner from "../assets/pythondeskbanner.jpg";
+import banner2 from "../assets/pythonmobilebanner.jpg";
 
 export default function Banner() {
   return (
     <div>
+      <img src={banner} alt="dbanner" className="hidden sm:block" />
       <img
-        src={banner}
-        alt="dbanner"
-        className="hidden sm:block"
-      />
-      <img
-        src="/src/assets/pythonmobilebanner.jpg"
+        src={banner2}
         alt="mbanner"
         className="block sm:hidden w-full h-auto"
       />

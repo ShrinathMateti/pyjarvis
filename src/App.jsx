4b10f6a40985e639280b3841c10b9055e16c939d
ShrinathMateti <shrinathrmateti@gmail.com>
2025-07-08 11:20:22 +0530

    Smooth Scroll Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; 
+import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
 import CoursePage from "./pages/CoursePage";
 import ContactPage from "./pages/ContactPage";
-
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/courses" element={<CoursePage/>} />
-        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/courses" element={<CoursePage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
